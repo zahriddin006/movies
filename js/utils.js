84edf1bec8c905ac1dd3410bc97547a052962064
zahriddin006 <zahriddin06@gmail.com>
2022-05-28 00:14:37 +0500

    utils js

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+// Function for query to element
+let $ = function (selector, node = document) {
+  return node.querySelector(selector);
+}
+
+// Function for create element
+let createElement = function(tagName, className, text) {
+  let element = document.createElement(tagName);
+  if (className) {
+    element.setAttribute("class", className);
+  }
+  if (text) {
+    element.textContent = text;
+  }
+
+  return element;
+}
